0d5af7d75bad3faf6eee551e8d51d7ef13483809
mrzero <101412753+mrzero1143@users.noreply.github.com>
2025-03-15 12:51:51 +0700

    Delete pdf.py
    delete

--- pdf.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-from PIL.ExifTags import TAGS
-
-image = Image.open("test.jpg")
-exif_data = image.getexif()
-exif_data[0x9286] = "MALWARE_TEST_STRING"  # Tambahkan string mencurigakan
-image.save("malicious_image.jpg", exif=exif_data)
